fcaba08b1873d985be3fcfa4b51b007b4aefb1ba
ttlv <gogophsersteven@gmail.com>
2022-01-19 21:10:43 +0800

    fix/8002: 修复结构体异常导致反序列化错误

--- response/express_query_request_8002_response.go
+++ response/express_query_request_8002_response.go
@@ -23,7 +23,7 @@ type ExpressQueryResponse struct {
 type Traces struct {
 	AcceptTime string `json:"AcceptTime"` // 轨迹发生时间，示例：2021-01-01 09:00:00
 	AcceptStation string `json:"AcceptStation"` // 轨迹描述
-	Location string `json:".Location"` // 当前城市
+	Location string `json:"Location"` // 当前城市
 	Action string `json:"Action"` // 增值物流状态： 0-暂无轨迹信息 1-已揽收 2-在途中 201-到达派件城市 202-派件中 211-已放入快递柜或驿站 3-已签收 301-正常签收 302-派件异常后最终签收 304-代收签收 311-快递柜或驿站签收 4-问题件 401-发货无信息 402-超时未签收 403-超时未更新 404-拒收(退件) 405-派件异常 406-退货签收 407-退货未签收 412-快递柜或驿站超时未取 10-待揽件
 	Remark string `json:"Remark"` // 备注
 }
